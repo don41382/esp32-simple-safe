77267da58f63d44db03adb35334779b6e8b60f08
Felix Eckhardt <eckhardt@rocket-solutions.de>
2021-01-11 14:38:03 +0100

    improved weekday calculation

--- src/lib/safe.cpp
+++ src/lib/safe.cpp
@@ -4,21 +4,22 @@ void printTime(tm &time) {
   Serial.println(&time, "%A, %B %d %Y %H:%M:%S");
 }
 
-int countDaysUntilFriday(tm * time) {
-  if (5 == time->tm_wday) {
+int countDaysUntilWeekDay(uint8_t targetWd, tm * time) {
+  uint8_t days = (targetWd - time->tm_wday) % 7;
+  if (days == 0) {
     return 7;
-  } else if (5 > time->tm_wday) {
-    return (5 - time->tm_wday);
   } else {
-    return 6;
+    return days;
   }
 }
 
+
 #define TIMEZONE 60 * 60 * 2
+#define TARGET_WEEKDAY 4 // Thursday
 
 time_t moveDateToFriday(time_t tt) {
   tm * time = localtime(&tt);
-  time->tm_sec += 60 * 60 * 24 * countDaysUntilFriday(time) - TIMEZONE;
+  time->tm_sec += 60 * 60 * 24 * countDaysUntilWeekDay(TARGET_WEEKDAY, time) - TIMEZONE;
   time->tm_hour = 12;
   time->tm_min = 00;
   return mktime(time);  
